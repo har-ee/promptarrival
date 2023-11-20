b760af5f37f4c120c2473e6325f880c5fead0558
Harry E <github@har.ee>
2023-11-20 22:49:30 +0000

    Move extraction function out of main

---
+++ extraction.py
@@ -0,0 +1,32 @@
+import re
+
+def extract_time_from_message(message):
+    time_pattern = re.compile(r'(?:in|be|brb|give\s*me)\s*(\d+|\d+\s*-\s*\d+)\s*(sec|secs|second|seconds|min|mins|minute|minutes|h|hour|hours)\s*(\d+)?', re.IGNORECASE)
+
+    match = time_pattern.search(message)
+
+    if match:
+        whole_str = match.group(0)
+        time_str = match.group(1).replace(' ', '')
+        time_units = match.group(2).lower()
+        time_str_extra = match.group(3)
+
+        if '-' in time_str:
+            time_value = time_str.split('-')[1]
+            time_seconds = int(time_value)
+        else:
+            time_seconds = int(time_str)
+
+        if 'min' in time_units or 'minute' in time_units:
+            time_seconds *= 60
+        elif 'h' in time_units or 'hour' in time_units:
+            time_seconds *= 3600
+            if time_str_extra is not None:
+                extra_mins = int(time_str_extra)
+                time_seconds += (extra_mins * 60)
+
+        print(time_str, time_units, time_seconds)
+
+        return time_seconds, whole_str.strip().lower()
+
+    return None, None
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 import re
 import time
 
+from extraction import extract_time_from_message
+
 # Load .env file
 load_dotenv()
 bot_token = os.getenv("DISCORD_TOKEN")
@@ -16,37 +18,6 @@ intents.messages = True
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
-
-def extract_time_from_message(message):
-    time_pattern = re.compile(r'(?:in|be|brb)\s*(\d+|\d+\s*-\s*\d+)\s*(sec|secs|second|seconds|min|mins|minute|minutes|h|hour|hours)\s*(\d+)?', re.IGNORECASE)
-
-    match = time_pattern.search(message)
-
-    if match:
-        whole_str = match.group(0)
-        time_str = match.group(1).replace(' ', '')
-        time_units = match.group(2).lower()
-        time_str_extra = match.group(3)
-
-        if '-' in time_str:
-            time_value = time_str.split('-')[1]
-            time_seconds = int(time_value)
-        else:
-            time_seconds = int(time_str)
-
-        if 'min' in time_units or 'minute' in time_units:
-            time_seconds *= 60
-        elif 'h' in time_units or 'hour' in time_units:
-            time_seconds *= 3600
-            if time_str_extra is not None:
-                extra_mins = int(time_str_extra)
-                time_seconds += (extra_mins * 60)
-
-        print(time_str, time_units, time_seconds)
-
-        return time_seconds, whole_str.strip()
-
-    return None, None
 
 @bot.event
 async def on_ready():
